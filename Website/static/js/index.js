002ce879b79a34c0aca2a3180aff959e68be6710
Lorna <kamaulorna@gmail.com>
2021-04-15 16:05:55 +0300

    style contact page

--- Website/static/js/index.js
+++ Website/static/js/index.js
@@ -1,1 +1,20 @@
 import '../scss/base.scss';
+
+//floating label animation for radio btns
+let checkDiv = document.getElementById("custom-float1");
+checkDiv.addEventListener("mouseleave", function() {
+    let floatingLabel =  document.getElementById("custom-floatingRadioLabel");
+    const rbs = document.querySelectorAll('input[name="interests"]');
+    let selectedValue;
+    for (const rb of rbs) {
+        if (rb.checked) {
+            selectedValue = rb.value;
+        } 
+    }
+    if (selectedValue){
+        floatingLabel.classList.add("float-custom-label");
+    } else if(floatingLabel.classList.contains(float-custom-label)){
+        // floatingLabel.classList.remove("float-custom-label");
+        floatingLabel.classList.add("unfloat-custom-label");
+    }    
+  }, false);

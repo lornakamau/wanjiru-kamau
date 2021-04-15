ca6d709be7d680ec998fed68dd1e2d631bc71b79
Lorna <kamaulorna@gmail.com>
2021-04-15 11:49:18 +0300

    add contact page

--- Website/urls.py
+++ Website/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$', views.home, name = 'home'),
+    path('', views.home, name = 'home'),
+    path('contact', views.contact, name = 'contact')
 ]
 
 if settings.DEBUG:

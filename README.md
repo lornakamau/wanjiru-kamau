35bdb8ce57ba2aeabda40b934441ccf69692dc4c
Lorna <kamaulorna@gmail.com>
2021-04-15 19:26:25 +0300

    add django resource to readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 npm start
 python3 manage.py runserver
+
+RESOURCES
+1. Setting up django email: https://data-flair.training/blogs/django-send-email/
--- Website/views.py
+++ Website/views.py
@@ -8,7 +8,6 @@ def home(request):
     return render(request, "home.html")
 
 def contact(request):       
-    # val = request.POST['firstname']
     if request.method == "POST":
         email = request.POST.get('email')
         firstname = request.POST.get('firstname')
@@ -18,7 +17,6 @@ def contact(request):
         fullname = firstname.title() + " " + lastname.title()
         
         send_mail("LornA EMAIL", message, email, [settings.EMAIL_HOST_USER], fail_silently=False)
-        # return render(request, 'contact.html', {'recepient': fullname})
         email_template = render_to_string('confirmation.html', {'name': fullname})
         email = EmailMessage(
             'Thank you for contacting Wanjiru Kamau',
@@ -28,5 +26,4 @@ def contact(request):
         )
         email.fail_silently = False
         email.send()
-        # print(email, fullname, interest, message)
     return render(request, "contact.html")

b34f5c8e84273a023d11d0a1875c9dc22a386ada
Lorna <kamaulorna@gmail.com>
2021-04-19 13:33:27 +0300

    style about image

--- Website/static/js/index.js
+++ Website/static/js/index.js
@@ -2,19 +2,33 @@ import '../scss/base.scss';
 
 //floating label animation for radio btns
 let checkDiv = document.getElementById("custom-float1");
-checkDiv.addEventListener("mouseleave", function() {
-    let floatingLabel =  document.getElementById("custom-floatingRadioLabel");
-    const rbs = document.querySelectorAll('input[name="interests"]');
-    let selectedValue;
-    for (const rb of rbs) {
-        if (rb.checked) {
-            selectedValue = rb.value;
-        } 
-    }
-    if (selectedValue){
-        floatingLabel.classList.add("float-custom-label");
-    } else if(floatingLabel.classList.contains(float-custom-label)){
-        // floatingLabel.classList.remove("float-custom-label");
-        floatingLabel.classList.add("unfloat-custom-label");
-    }    
-  }, false);
+if (checkDiv){
+    checkDiv.addEventListener("mouseleave", function() {
+        let floatingLabel =  document.getElementById("custom-floatingRadioLabel");
+        const rbs = document.querySelectorAll('input[name="interests"]');
+        let selectedValue;
+        for (const rb of rbs) {
+            if (rb.checked) {
+                selectedValue = rb.value;
+            } 
+        }
+        if (selectedValue){
+            floatingLabel.classList.add("float-custom-label");
+        } else if(floatingLabel.classList.contains(float-custom-label)){
+            floatingLabel.classList.add("unfloat-custom-label");
+        }    
+      }, false);
+}
+
+//set/unset grayscale for about image
+function getScroll(){
+    var scrollLeft = (window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
+    var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+    var aboutImage = document.getElementsByClassName("about-image");
+    if (scrollTop >  window.innerWidth * .7 && aboutImage[0]){
+        aboutImage[0].style.filter = "grayscale(100%)";
+    } else if (scrollTop <  window.innerWidth * .7 && aboutImage[0]) {
+        aboutImage[0].style.filter = "grayscale(0%)";
+    }}
+document.body.onscroll = getScroll;
